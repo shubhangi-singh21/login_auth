de88730c801f00f914e973a76651caa00269ebd2
shubhangi-singh21 <shubhangifas@gmail.com>
2020-09-17 17:02:16 +0530

    added animation〰

--- lib/screens/auth/auth.dart
+++ lib/screens/auth/auth.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:login_page_auth/screens/background_painter.dart';
 
-class AuthScreen extends StatelessWidget {
+class AuthScreen extends StatefulWidget {
   const AuthScreen({Key key}) : super(key: key);
+
+  @override
+  _AuthScreenState createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _controller =
+        AnimationController(vsync: this, duration: Duration(seconds: 2));
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,9 +27,20 @@ class AuthScreen extends StatelessWidget {
       body: Stack(
         children: [
           SizedBox.expand(
-              child: CustomPaint(
-            painter: BackgroundPainter(),
-          ))
+            child: CustomPaint(
+              painter: BackgroundPainter(
+                animation: _controller.view,
+              ),
+            ),
+          ),
+          Center(
+            child: RaisedButton(
+              onPressed: () {
+                _controller.forward(from: 0);
+              },
+              child: Text('Animate'),
+            ),
+          )
         ],
       ),
     );
--- lib/screens/background_painter.dart
+++ lib/screens/background_painter.dart
@@ -1,8 +1,12 @@
+import 'dart:math';
+import 'dart:ui';
+
+import 'package:flutter/animation.dart';
 import 'package:flutter/rendering.dart';
 import 'package:login_page_auth/config/palette.dart';
 
 class BackgroundPainter extends CustomPainter {
-  BackgroundPainter()
+  BackgroundPainter({Animation<double> animation})
       : bluePaint = Paint()
           ..color = Palette.darkBlue
           ..style = PaintingStyle.fill,
@@ -11,7 +15,41 @@ class BackgroundPainter extends CustomPainter {
           ..style = PaintingStyle.fill,
         yellowPaint = Paint()
           ..color = Palette.yellow
-          ..style = PaintingStyle.fill;
+          ..style = PaintingStyle.fill,
+        liquidAnim = CurvedAnimation(
+          curve: Curves.elasticOut,
+          reverseCurve: Curves.easeInBack,
+          parent: animation,
+        ),
+        yellowAnim = CurvedAnimation(
+          parent: animation,
+          curve: const Interval(
+            0,
+            0.7,
+            curve: Interval(0, 0.8, curve: SpringCurve()),
+          ),
+          reverseCurve: Curves.linear,
+        ),
+        seablueAnim = CurvedAnimation(
+          parent: animation,
+          curve: const Interval(
+            0,
+            0.8,
+            curve: Interval(0, 0.9, curve: SpringCurve()),
+          ),
+          reverseCurve: Curves.easeInCirc,
+        ),
+        blueAnim = CurvedAnimation(
+          parent: animation,
+          curve: const SpringCurve(),
+          reverseCurve: Curves.easeInCirc,
+        ),
+        super(repaint: animation);
+
+  final Animation<double> liquidAnim;
+  final Animation<double> blueAnim;
+  final Animation<double> seablueAnim;
+  final Animation<double> yellowAnim;
 
   final Paint bluePaint;
   final Paint seabluePaint;
@@ -21,6 +59,8 @@ class BackgroundPainter extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     print('printing');
     paintBlue(canvas, size);
+    paintSeaBlue(canvas, size);
+    paintYellow(canvas, size);
   }
 
   void paintBlue(Canvas canvas, Size size) {
@@ -28,13 +68,87 @@ class BackgroundPainter extends CustomPainter {
     path.moveTo(size.width, size.height / 2);
     path.lineTo(size.width, 0);
     path.lineTo(0, 0);
+    path.lineTo(
+      0,
+      lerpDouble(0, size.height, blueAnim.value),
+    );
     _addPointsToPath(path, [
-      Point(0, 0),
-      Point(size.width / 2, size.height / 2),
-      Point(size.width, size.height / 2),
+      Point(
+        lerpDouble(0, size.width / 3, blueAnim.value),
+        lerpDouble(0, size.height, blueAnim.value),
+      ),
+      Point(
+        lerpDouble(size.width / 2, size.width / 4 * 3, liquidAnim.value),
+        lerpDouble(size.height / 2, size.height / 4 * 3, liquidAnim.value),
+      ),
+      Point(
+        size.width,
+        lerpDouble(size.height / 2, size.height * 3 / 4, liquidAnim.value),
+      ),
     ]);
-
     canvas.drawPath(path, bluePaint);
+  }
+
+  void paintSeaBlue(Canvas canvas, Size size) {
+    final path = Path();
+    path.moveTo(size.width, 300);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+    path.lineTo(
+      0,
+      lerpDouble(size.height / 4, size.height / 2, seablueAnim.value),
+    );
+    _addPointsToPath(
+      path,
+      [
+        Point(
+          size.width / 4,
+          lerpDouble(size.height / 2, size.height * 3 / 4, liquidAnim.value),
+        ),
+        Point(
+          size.width * 3 / 5,
+          lerpDouble(size.height / 4, size.height / 2, liquidAnim.value),
+        ),
+        Point(
+          size.width * 4 / 5,
+          lerpDouble(size.height / 6, size.height / 3, seablueAnim.value),
+        ),
+        Point(
+          size.width,
+          lerpDouble(size.width / 5, size.height / 4, seablueAnim.value),
+        ),
+      ],
+    );
+
+    canvas.drawPath(path, seabluePaint);
+  }
+
+  void paintYellow(Canvas canvas, Size size) {
+    if (yellowAnim.value > 0) {
+      final path = Path();
+      path.moveTo(size.width * 3 / 4, 0);
+      path.lineTo(0, 0);
+      path.lineTo(
+        0,
+        lerpDouble(0, size.height / 12, yellowAnim.value),
+      );
+      _addPointsToPath(path, [
+        Point(
+          size.width / 7,
+          lerpDouble(0, size.height / 6, liquidAnim.value),
+        ),
+        Point(
+          size.width / 3 * 2,
+          lerpDouble(0, size.height / 8, liquidAnim.value),
+        ),
+        Point(
+          size.width / 3 * 4,
+          0,
+        ),
+      ]);
+
+      canvas.drawPath(path, yellowPaint);
+    }
   }
 
   @override
@@ -64,5 +178,22 @@ class BackgroundPainter extends CustomPainter {
 class Point {
   final double x;
   final double y;
+
   Point(this.x, this.y);
+}
+
+//custom curve to give gooey spring effect
+class SpringCurve extends Curve {
+  const SpringCurve({
+    this.a = 0.15,
+    this.w = 19.4,
+  });
+
+  final double a;
+  final double w;
+
+  @override
+  double transformInternal(double t) {
+    return ((-pow(e, -t / a) * cos(t * w)) + 1).toDouble();
+  }
 }
